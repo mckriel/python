8ab6521a30c3e5b6bf88c77ba542a5bf5b61b2db
Matthew Kriel <mckriel@gmail.com>
2023-04-13 19:11:54 +0200

    started over

--- console-apps/coffee-machine/main.py
+++ console-apps/coffee-machine/main.py
@@ -1,9 +1,15 @@
 import menu
 import resources
 
-menu = menu.MENU
-resources = resources.resources
-cash_on_hand = 0
+
+def initialise_coffee_machine():
+    global machine_resources
+    global menu
+    global cash_on_hand
+
+    machine_resources = resources.resources
+    menu = menu.MENU
+    cash_on_hand = 0
 
 
 def resources_report(current_resources):
@@ -14,25 +20,36 @@ def resources_report(current_resources):
 
 def check_resources_for_drink(drink_resources):
     for item in drink_resources:
-        if drink_resources[item] > resources[item]:
+        if drink_resources[item] > machine_resources[item]:
             print(f'Not enough {item.capitalize()} to complete order.')
             return False
     return True
+
+
+def update_cash_on_hand(cash_paid):
+    global cash_on_hand
+    cash_on_hand += cash_paid
 
 
 def process_coins(drink_cost):
     total = int(input('How many Quarters?: ')) * 0.25
     total += int(input('How many Dimes?: ')) * 0.10
 
+    if total > drink_cost:
+        change = round(total - drink_cost, 2)
+        print(f'You will be refunded: ${change:.2f}')
+
     if total >= drink_cost:
+        update_cash_on_hand(cash_paid=total)
         return True
     print(f'The drink costs ${drink_cost}, however the total value of coins inserted was ${total}. Please try again.')
     return False
 
 
 def main():
+
+    initialise_coffee_machine()
     continue_operations = True
-    print(resources)
 
     while continue_operations:
         user_choice = input('What would you like? (espresso/latte/cappuccino): ')
@@ -40,13 +57,12 @@ def main():
         if user_choice == 'off':
             break
         elif user_choice == 'report':
-            resources_report(current_resources=resources)
+            resources_report(current_resources=machine_resources)
         else:
             drink = menu[user_choice]
             if check_resources_for_drink(drink_resources=drink['ingredients']):
                 if process_coins(drink_cost=drink['cost']):
-                    
-
+                    print('sweet')
 
 
 main()
