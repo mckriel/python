d68d0a57874b3a67ea10e303bf48a10c300a230c
Matthew Kriel <mckriel@gmail.com>
2023-04-26 22:21:28 +0200

    snake finalised

--- turtle-apps/snake-game/main.py
+++ turtle-apps/snake-game/main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title('Snake')
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -23,6 +27,19 @@ while game_is_on:
 
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        print('nom')
+        food.refresh()
+        snake.extend()
+        scoreboard.update_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
