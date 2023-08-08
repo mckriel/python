5e1d1e2545058ed2fab394095ef802ecaeafbed9
Matthew Kriel <mckriel@gmail.com>
2023-08-08 18:14:51 +0200

    uncommented % difference check

--- apps/api/stockmarket/main.py
+++ apps/api/stockmarket/main.py
@@ -41,7 +41,7 @@ def get_pricing():
     closing_price_yesterday = data_list[0]['4. close']
     closing_price_day_before = data_list[1]['4. close']
     difference_value = float(closing_price_yesterday) - float(closing_price_day_before)
-    difference_percentage = 7 #((float(closing_price_yesterday)/float(closing_price_day_before)) * 100) - 100
+    difference_percentage = ((float(closing_price_yesterday)/float(closing_price_day_before)) * 100) - 100
 
     print(f'Company: {COMPANY_NAME} ({STOCK_NAME})')
     print(f'Closing price yesterday: ${closing_price_yesterday}')
@@ -54,35 +54,3 @@ def get_pricing():
 
 
 get_pricing()
-
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
